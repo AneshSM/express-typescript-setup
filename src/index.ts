4ce464304f367a14c849797953cc580ecddf9425
Anesh SM <aneshsm204@gmail.com>
2023-08-15 23:44:23 +0530

    Updated with syntax checker, formatter and tester-V2

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,46 @@
-import express, { Express, Request, Response } from 'express';
-import bodyParser from 'body-parser';
+import express, {
+  Express,
+  Request,
+  Response,
+  ErrorRequestHandler,
+  NextFunction,
+} from 'express';
 import dotenv from 'dotenv-safe';
-
+import helmet from 'helmet';
 import add from '@src/add/add';
+import createHttpError from 'http-errors';
 
 //* Loads all variables defined in the .env to node environment process.env
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT: number = Number(process.env.PORT) || 3000;
 const app: Express = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('<h1>Hi Express TypeScript Backend was Successful</h1>');
 });
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(new createHttpError.NotFound());
+});
+
+const errorHandler: ErrorRequestHandler = (
+  err,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  res.status(err.status || 500).send({
+    status: err.status || 500,
+    message: err.message,
+  });
+};
+
+app.use(errorHandler);
 
 console.log(add(1, 4));
 
